bb829115af7488e60092048196042e20723f9ba8
dmitrybannikov <prestiz4321@gmail.com>
2023-05-05 07:56:38 -0700

    try emulator

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# go_router_advantage
-go_router_advantage
-
-Deep dive in go_router , full advantage go_router
 
--- example_3/lib/main_emulator.dart
+++ example_3/lib/main_emulator.dart
@@ -1,10 +1,7 @@
 import 'package:authentication_repository/authentication_repository.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:bloc/bloc.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import '/app/app.dart';
 import 'app/utils/print_colors.dart';
