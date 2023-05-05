bd38239d3a20bb023ad8bb2a772696a134a0c9a4
dmitrybannikov <prestiz4321@gmail.com>
2023-05-05 07:51:25 -0700

    try emulator

---
+++ example_3/lib/main_emulator.dart
@@ -0,0 +1,32 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import '/app/app.dart';
+import 'app/utils/print_colors.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = const AppBlocObserver();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final _firebaseAuth = FirebaseAuth.instance;
+  try {
+    // FirebaseFirestore.instance.useFirestoreEmulator('localhost', 9099);
+    await _firebaseAuth.useAuthEmulator('localhost', 9099);
+    debugPrint('$green success runnning Emulator $reset');
+  } catch (e) {
+    debugPrint('$red $e $reset');
+  }
+
+  final authenticationRepository =
+      AuthenticationRepository(firebaseAuth: _firebaseAuth);
+  await authenticationRepository.user.first;
+
+  runApp(App(authenticationRepository: authenticationRepository));
+}
